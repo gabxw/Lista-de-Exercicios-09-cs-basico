db6318d28a0d9735b50d7a55a742d6e1c9893ee0
gabrieljmaciel1 <33903850-gabrieljmaciel1@users.noreply.replit.com>
2025-02-11 13:55:17 +0000

    User checkpoint: Fix: Adjust student grade input to only include two grades per student.

--- Lista-de-Exercicios-09-cs-basico/exercicio02.cs
+++ Lista-de-Exercicios-09-cs-basico/exercicio02.cs
@@ -6,8 +6,8 @@ namespace Lista_de_Exercicios_08_cs_basico
     // Classe Pessoa
     class Pessoa
     {
-        public string Nome { get; set; } // Propriedade para o nome
-        public int[] Notas { get; set; } // Propriedade para as notas
+        public string Nome { get; set; }
+        public int[] Notas { get; set; } 
 
         public Pessoa()
         {
@@ -20,15 +20,15 @@ namespace Lista_de_Exercicios_08_cs_basico
     {
         static void Main(string[] args)
         {
-            // Criando uma lista de pessoas
+           
             List<Pessoa> pessoas = new List<Pessoa>();
 
-            // Adicionando pessoas à lista
-            pessoas.Add(new Pessoa { Nome = "Gabriel", Notas = new int[] { 8, 9, 7 } });
-            pessoas.Add(new Pessoa { Nome = "Matheus", Notas = new int[] { 10, 2, 7 } });
-            pessoas.Add(new Pessoa { Nome = "Athur", Notas = new int[] { 4, 5, 8 } });
-            pessoas.Add(new Pessoa { Nome = "Borba", Notas = new int[] { 3, 9, 5 } });
-            pessoas.Add(new Pessoa { Nome = "Ana", Notas = new int[] { 6, 1, 5 } });
+            
+            pessoas.Add(new Pessoa { Nome = "Gabriel", Notas = new int[] { 8, 9} });
+            pessoas.Add(new Pessoa { Nome = "Matheus", Notas = new int[] { 10, 2 } });
+            pessoas.Add(new Pessoa { Nome = "Athur", Notas = new int[] { 4, 5} });
+            pessoas.Add(new Pessoa { Nome = "Borba", Notas = new int[] { 3, 9 } });
+            pessoas.Add(new Pessoa { Nome = "Ana", Notas = new int[] { 6, 1} });
 
             // Exibindo as notas de cada pessoa
             foreach (var pessoa in pessoas)
