e2ddad82e2ea494e8c8051158e6e22424e6f6457
Gabriel José Maciel <gabriel.jmaciel13@gmail.com>
2025-02-12 14:03:54 +0000

    Assistant checkpoint: Remove duplicate file and rename class to Main
    Assistant generated file changes:
    - Lista-de-Exercicios-09-cs-basico/exercicio02/exercicio02.cs: Update class name to Main
    
    ---
    
    User prompt:
    
    Running the code in my project results in the following:
    
    ```
    /home/runner/workspace/Lista-de-Exercicios-09-cs-basico/exercicio02/exercicio02.cs(9,20): error CS0101: The namespace 'Lista_de_Exercicios_08_cs_basico' already contains a definition for 'Exercicio02' [/home/runner/workspace/Lista-de-Exercicios-09-cs-basico/Lista-de-Exercicios-08-cs-basico.csproj]
    /home/runner/workspace/Lista-de-Exercicios-09-cs-basico/exercicio02/exercicio02.cs(11,21): error CS0111: Type 'Exercicio02' already defines a member called 'Main' with the same parameter types [/home/runner/workspace/Lista-de-Exercicios-09-cs-basico/Lista-de-Exercicios-08-cs-basico.csproj]
    
    The build failed. Fix the build errors and run again.
    
    ```
    
    If applicable, propose a fix immediately.

--- Lista-de-Exercicios-09-cs-basico/exercicio02/exercicio02.cs
+++ Lista-de-Exercicios-09-cs-basico/exercicio02/exercicio02.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Lista_de_Exercicios_08_cs_basico
 {
-    internal class Exercicio02
+    internal class Main
     {
         static void Main(string[] args)
         {
